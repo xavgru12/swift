d09e4e208f414f9c917f876eb34289413fd0fb1e
Xaver Gruber <xaver.max.gruber@gmail.com>
2025-05-03 17:29:20 +0200

    fix indendation

--- lib/Sema/TypeCheckDecl.cpp
+++ lib/Sema/TypeCheckDecl.cpp
@@ -3091,18 +3091,18 @@ llvm::SmallVector<swift::GenericTypeParamType *, 4> mutableTypealiasParams(
   if (mutableNominalParams.size() != mutableTypealiasParams.size())
     return false;
   else{
-      if(mutableNominalParams.size() != 0 && mutableTypealiasParams.size() != 0)
-        return isTypeInferredByTypealias(typealias, nominal);
+    if(mutableNominalParams.size() != 0 && mutableTypealiasParams.size() != 0)
+      return isTypeInferredByTypealias(typealias, nominal);
 
       if (std::equal(mutableNominalParams.begin(), mutableNominalParams.end(),
                    mutableTypealiasParams.begin(),
                   [](GenericTypeParamType *gp1, GenericTypeParamType *gp2) {
                     return gp1->isEqual(gp2);
                   }))
-    return isTypeInferredByTypealias(typealias, nominal);
-  else{
-      return false;
-    }
+        return isTypeInferredByTypealias(typealias, nominal);
+      else{
+          return false;
+      }
   }
   // If neither is generic at this level, we have a pass-through typealias.
   if (!typealias->isGeneric()) return true;
